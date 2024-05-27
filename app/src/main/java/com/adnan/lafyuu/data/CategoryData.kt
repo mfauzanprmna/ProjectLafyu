ae7dc30fca3b26bfc88da5d618f35a8434c0416e
AdnanMAF <adnanfadillah50@gmail.com>
2024-05-27 12:30:05 +0700

    Initial commit

---
+++ app/src/main/java/com/adnan/lafyuu/data/CategoryData.kt
@@ -0,0 +1,6 @@
+package com.adnan.lafyuu.data
+
+data class CategoryData(
+    val name: String,
+    val imageId: Int
+)
---
+++ app/src/main/java/com/adnan/lafyuu/data/ItemData.kt
@@ -0,0 +1,7 @@
+package com.adnan.lafyuu.data
+
+data class ItemData(
+    var name: String,
+    var price: String,
+    var imageId: Int
+)
